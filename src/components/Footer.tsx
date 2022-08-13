c4df0824615f5867f67634b1747faef47d2d44c6
Jorge Munoz <jmunoz241995@gmail.com>
2022-08-13 14:00:14 -0700

    button icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,25 @@
+import TwitterIcon from '../assets/Twitter Icon.png';
+import FacebookIcon from '../assets/Facebook Icon.png';
+import InstaIcon from '../assets/Instagram Icon.png';
+import GithubIcon from '../assets/GitHub Icon.png';
+
 function Footer() {
-    return <div></div>;
+    return (
+        <div className='footer'>
+            <button>
+                <img src={TwitterIcon} />
+            </button>
+            <button>
+                <img src={FacebookIcon} />
+            </button>
+            <button>
+                <img src={InstaIcon} />
+            </button>
+            <button>
+                <img src={GithubIcon} />
+            </button>
+        </div>
+    );
 }
 
 export default Footer;
